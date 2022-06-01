e90a7b2cf8b53e11fff7b428063693ded087096b
lightforward <1294528130@qq.com>
2022-06-01 11:14:16 +0800

    修改:

---
+++ echartsDemo/src/main/java/com/example/user/test/Test.java
@@ -0,0 +1,28 @@
+package com.example.user.test;
+
+/**
+ *
+ * 第1个人10，第2个比第1个人大2岁，依次递推，请用递归方式计算出第8个人多大?
+ *
+ * @author wangns
+ *
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println(getComputeAge(8));
+    }
+
+    public static int getComputeAge(int n) {
+        int age = 0;
+        if (n == 1) {
+            age = 10;
+        } else {
+            age = getComputeAge(n - 1) + 2;
+        }
+        return age;
+    }
+
+
+}
